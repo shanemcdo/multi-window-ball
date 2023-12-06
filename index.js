8765bf7e38b55d927f701e38ede833c446c0dbbf
Shane McDonough <shanemcdman@gmail.com>
2023-12-06 13:34:14 -0800

    Have different balls for each window

--- index.js
+++ index.js
@@ -3,7 +3,21 @@
 const svgns = "http://www.w3.org/2000/svg";
 const svg = document.querySelector('svg');
 const circleRadius = 50;
-let isCircleController = true;
+const idPrefix = 'circleInfo-';
+const colors = [
+	'rgb(255, 0, 0)',
+	'rgb(255, 128, 0)',
+	'rgb(255, 255, 0)',
+	'rgb(128, 255, 0)',
+	'rgb(0, 255, 0)',
+	'rgb(0, 255, 128)',
+	'rgb(0, 255, 255)',
+	'rgb(0, 128, 255)',
+	'rgb(0, 0, 255)',
+	'rgb(128, 0, 255)',
+	'rgb(255, 0, 255)',
+	'rgb(255, 0, 128)'
+];
 
 class Circle {
 	constructor(x, y, radius) {
@@ -12,6 +26,10 @@ class Circle {
 		this.y = y;
 		this.r = radius;
 		svg.appendChild(this.el);
+	}
+
+	remove() {
+		svg.removeChild(this.el);
 	}
 
 	set(key, val) {
@@ -63,15 +81,44 @@ class Circle {
 	}
 }
 
-function clearOldStorage() {
-	const info = getCircleInfo()
-	if(Date.now() - info.lastUpdated > 1000) {
-		window.localStorage.removeItem('circleInfo');
-	}
+function getId() {
+	const ids = Object.keys(window.localStorage)
+		.filter(key => key.startsWith(idPrefix))
+		.map(key => parseInt(key.replace(idPrefix, '')))
+		.toSorted();
+	return ids.at(-1) + 1 || 0
 }
 
-function getCircleInfo() {
-	return JSON.parse(window.localStorage.getItem('circleInfo')) ?? {
+function getCircleInfos() {
+	return Object.entries(window.localStorage)
+		.filter(([key]) => key.startsWith(idPrefix))
+		.map(([key, value]) => [key, JSON.parse(value)]);
+}
+
+function getCircleInfosArray() {
+	const result = [];
+	getCircleInfos()
+		.map(([key, value]) => [parseInt(key.replace(idPrefix, '')), value])
+		.forEach(([i, value]) => {
+			result[i] = value
+		});
+	return result;
+}
+
+function removeCircleInfo(id) {
+	window.localStorage.removeItem(`${idPrefix}${id}`);
+}
+
+function clearOldStorage() {
+	getCircleInfos().forEach(([key, value]) => {
+		if(Date.now() - value.lastUpdated > 1000) {
+			window.localStorage.removeItem(key);
+		}
+	});
+}
+
+function getDefaultCircleInfo() {
+	return {
 		pos: {
 			x: 50,
 			y: 50,
@@ -122,8 +169,8 @@ function moveCircle(circleInfo, screenInfo) {
 	circleInfo.lastUpdated = Date.now();
 }
 
-function setCircleInfo(circleInfo) {
-	window.localStorage.setItem('circleInfo', JSON.stringify(circleInfo));
+function setCircleInfo(id, circleInfo) {
+	window.localStorage.setItem(`${idPrefix}${id}`, JSON.stringify(circleInfo));
 }
 
 function drawCircle(circle, circleInfo, screenInfo) {
@@ -133,17 +180,44 @@ function drawCircle(circle, circleInfo, screenInfo) {
 	};
 }
 
+function max(a, b) {
+	return a > b ? a : b;
+}
+
 function main(){
-	const circle = new Circle(-100, -100, circleRadius);
+	const circles = [];
+	clearOldStorage();
+	const id = getId();
+	console.log(id);
+	window.onbeforeunload = () => {
+		removeCircleInfo(id);
+	};
 	setInterval(() => {
 		clearOldStorage();
-		const circleInfo = getCircleInfo();
 		const screenInfo = getScreenInfo();
-		if(isCircleController) {
-			moveCircle(circleInfo, screenInfo);
-			setCircleInfo(circleInfo);
+		const circleInfos = getCircleInfosArray();
+		if(circleInfos[id] === undefined) {
+			circleInfos[id] = getDefaultCircleInfo();
 		}
-		drawCircle(circle, circleInfo, screenInfo);
+		moveCircle(circleInfos[id], screenInfo);
+		setCircleInfo(id, circleInfos[id]);
+		const len = max(circleInfos.length, circles.length);
+		for(let i = 0; i < len; i++) {
+			if(circleInfos[i] === undefined) {
+				console.log(circleInfos);
+				console.log(circles);
+				if(circles[i] !== undefined) {
+					circles[i].remove();
+					circles[i] = undefined;
+				}
+				continue;
+			}
+			if(circles[i] === undefined) {
+				circles[i] = new Circle(-100, -100, circleRadius);
+				circles[i].set('fill', colors[i % colors.length]);
+			}
+			drawCircle(circles[i], circleInfos[i], screenInfo);
+		}
 	}, 10);
 }
 
